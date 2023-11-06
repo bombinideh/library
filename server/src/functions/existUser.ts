e73cc38938f87d73624236a085abf9abbe1aab3e
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-11-05 21:25:14 -0300

    fix: remove promise resolution in get user function

--- server/src/functions/existUser.ts
+++ server/src/functions/existUser.ts
@@ -2,8 +2,8 @@ import { database } from "../knex";
 import { IUser } from "../models/User";
 
 export const existUser = async (param: IUser["email"] | IUser["user_id"]) => {
-  const filter =
-    typeof param === "string" ? { email: param } : { user_id: param };
+  const filter = 
+  typeof param === "string" ? { email: param } : { user_id: param };
 
-  return await database("users").select("*").where(filter).first();
+  return database("users").select("*").where(filter).first();
 };
