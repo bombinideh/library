44bedce68b53a70be9827ac15a6bff4d894082bc
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-11-05 21:15:55 -0300

    chore: add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,13 @@ import "dotenv/config";
 import { database } from "./knex";
 import { readdirSync } from "fs";
 import { join } from "path";
+import cors from "cors";
 
 const app = Express();
 const PORT = Number(process.env.PORT) || 3000;
 const routes = readdirSync(join(__dirname, "routes"));
 
+app.use(cors());
 app.use(Express.json());
 app.listen(`${PORT}`, () => console.log(`🚀 Server running on port ${PORT}`));
 
