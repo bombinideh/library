df1a6c83a3ade24536271d95dae4a99eae10c72c
Deborah Bombini <dehhbombini@gmail.com>
2023-11-14 19:28:28 -0300

    :wrench: add foreign keys

--- server/database/migrations/20231011005849_create_table_shelfs.ts
+++ server/database/migrations/20231011005849_create_table_shelfs.ts
@@ -1,11 +1,20 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable("shelfs", (table) => {
+  await knex.schema.createTable("shelfs", table => {
     table.increments("shelf_id").primary().notNullable();
-    table.string("name").unique().notNullable();
+    table.string("name").notNullable();
+    table.integer("bookcase_id").notNullable();
+    table.boolean("active").notNullable().defaultTo(true);
     table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
     table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
+
+    table
+      .foreign(["bookcase_id"])
+      .references(["bookcase_id"])
+      .inTable("bookcases")
+      .onDelete("cascade")
+      .onUpdate("cascade");
   });
 }
 
--- server/database/migrations/20231011010406_create_table_boxes.ts
+++ server/database/migrations/20231011010406_create_table_boxes.ts
@@ -1,12 +1,20 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable("boxes", (table) => {
+  await knex.schema.createTable("boxes", table => {
     table.increments("box_id").primary().notNullable();
-    table.string("name").unique().notNullable();
+    table.string("name").notNullable();
     table.boolean("active").notNullable().defaultTo(true);
+    table.integer("shelf_id").notNullable();
     table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
     table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
+
+    table
+      .foreign(["shelf_id"])
+      .references(["shelf_id"])
+      .inTable("shelfs")
+      .onDelete("cascade")
+      .onUpdate("cascade");
   });
 }
 
