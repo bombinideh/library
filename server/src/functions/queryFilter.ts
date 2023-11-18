d170248dc9a91ab15152cd1fde388b4be1187bd3
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-11-18 20:22:50 -0300

    fix: update queryFilter helper

--- server/src/functions/queryFilter.ts
+++ server/src/functions/queryFilter.ts
@@ -1,42 +1,32 @@
-import { Request } from "express";
 import { database } from "../knex";
 
 interface QueryParams {
-  items: number;
-  page: number;
+  items?: number;
+  page?: number;
   searchColumn?: string;
   searchQuery?: string;
 }
 
 interface PaginationProps {
-  queryParams: Request["query"];
+  queryParams: QueryParams;
   table: string;
 }
 
-const defaultQueryParams: QueryParams = {
-  items: 10,
-  page: 1,
-  searchColumn: undefined,
-  searchQuery: undefined,
-};
-
 export async function queryFilter({ queryParams, table }: PaginationProps) {
-  const { items, page, searchColumn, searchQuery } = {
-    ...defaultQueryParams,
-    ...queryParams,
-  };
-  const query = database(table)
-    .limit(items)
-    .offset(items * (page - 1));
-  let finalQuery = query;
+  const { items, page, searchColumn, searchQuery } = queryParams;
+  let query = database(table);
+
+  if (items) query = query.limit(items);
+
+  if (items && page) query = query.offset(items * (page - 1));
 
   if (searchColumn && searchQuery)
-    finalQuery = query.where(searchColumn, "like", `%${searchQuery}%`);
+    query = query.whereILike(searchColumn, `%${searchQuery}%`);
 
   const totalItems = await database(table).count("*").first();
 
   return {
-    query: finalQuery,
+    query,
     total: Number(totalItems?.count),
   };
 }
