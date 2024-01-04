aa9fad5800bfd5cb060df1edda2bc56bae17cbd5
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-01-04 20:43:45 -0300

    chore: add enviroment variables in client-side

--- client/src/config/index.tsx
+++ client/src/config/index.tsx
@@ -1,4 +1,4 @@
 export const appName = "Centro Espírita Amor e Caridade";
 export const appNameAcronym = "CEAC";
 export const appPrivateName = "Acervo histórico";
-export const apiURL = "http://localhost:50002";
+export const appApiURL = import.meta.env.APP_API_URL;
